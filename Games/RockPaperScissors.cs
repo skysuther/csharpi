65cc4b905bccd7532ceb8443f9da1641f7b410cf
Skyler Sutherland <skysuther@gmail.com>
2023-05-17 23:22:51 -0400

    Add rematch
    Add in function to rematch and play another game!

--- Games/RockPaperScissors.cs
+++ Games/RockPaperScissors.cs
@@ -273,72 +273,100 @@ namespace csharpi.rockpaperscissors
                 case "scissors-id":
                     answer = "scissors";
                     break;
-            }
-            // Set up object to add to list
-            Tuple<SocketUser, string> useranswer = Tuple.Create(component.User,answer);
-            // Only one answer per user, add object useranser to list
-            // if(UserAnswers.Count() == 0)
-            // {
-            //     UserAnswers.Add(useranswer);
-            // } else
-            // {
-            //     if(UserAnswers[0].Item1.Username.Equals(component.User.Username))
-            //     {
-            //     } else
-            //     {
-            //         UserAnswers.Add(useranswer);
-            //     }
-            // }
-            UserAnswers.Add(useranswer);
-            var message = "";
-            var WinStatus = "win";
+                case "rematch-id":
+                    var rockbuilder = new ComponentBuilder()
+                        .WithButton("Rock", "rock-id")
+                        .WithButton("Paper", "paper-id")
+                        .WithButton("Scissors", "scissors-id");
+                    
+                    var message = "Choose to play!";
 
-            if (UserAnswers.Count() >= 2)
-            {
-                switch(UserAnswers[0].Item2)
-                {
-                    case "rock":
-                    if (UserAnswers[1].Item2 == "paper") {WinStatus = "lost";}
-                    if (UserAnswers[1].Item2 == "rock") {WinStatus = "tie";}
+                    await component.UpdateAsync(x =>
+                        { 
+                            x.Content = $"{message}";
+                            x.Components = rockbuilder.Build();
+                        });
                     break;
-                    case "paper":
-                    if (UserAnswers[1].Item2 == "scissors") {WinStatus = "lost";}
-                    if (UserAnswers[1].Item2 == "paper") {WinStatus = "tie";}
-                    break;
-                    case "scissors":
-                    if (UserAnswers[1].Item2 == "rock") {WinStatus = "lost";}
-                    if (UserAnswers[1].Item2 == "scissors") {WinStatus = "tie";}
-                    break;
-                }
-                if (WinStatus == "win")
-                {
-                    message = $"[**{UserAnswers[0].Item1.Username}**] has won >> [**{UserAnswers[0].Item2}**]\n";
-                    message += $"[**{UserAnswers[1].Item1.Username}**] has lost >> [**{UserAnswers[1].Item2}**]\n";
-                } else if (WinStatus == "tie")
-                {
-                    message = $"[**{UserAnswers[0].Item1.Username}**] >> [**{UserAnswers[0].Item2}**]\n";
-                    message += $"[**{UserAnswers[1].Item1.Username}**] >> [**{UserAnswers[1].Item2}**]\n";
-                    message += $"Its a tie!";
-                } else
-                {
-                    message = $"[**{UserAnswers[1].Item1.Username}**] has won >> [**{UserAnswers[1].Item2}**]\n";
-                    message += $"[**{UserAnswers[0].Item1.Username}**] has lost >> [**{UserAnswers[0].Item2}**]\n";
-                }
-                await component.UpdateAsync(x =>
-                { 
-                    x.Content = $"{message}";
-                    x.Components = null;
-                });
-
-                UserAnswers.Clear();
-            } else 
-            {
-                await component.UpdateAsync(x =>
-                { 
-                    x.Content = $"{component.User.Username} has chosen!\n";
-                });
             }
 
+            if (validAnswerIds.Contains(component.Data.CustomId)) {
+                // Set up object to add to list
+                Tuple<SocketUser, string> useranswer = Tuple.Create(component.User,answer);
+                // Only one answer per user, add object useranser to list
+                // if(UserAnswers.Count() == 0)
+                // {
+                //     UserAnswers.Add(useranswer);
+                // } else
+                // {
+                //     if(UserAnswers[0].Item1.Username.Equals(component.User.Username))
+                //     {
+                //     } else
+                //     {
+                //         UserAnswers.Add(useranswer);
+                //     }
+                // }
+                UserAnswers.Add(useranswer);
+                var message = "";
+                var WinStatus = "win";
+
+                if (UserAnswers.Count() >= 2)
+                {
+                    switch(UserAnswers[0].Item2)
+                    {
+                        case "rock":
+                        if (UserAnswers[1].Item2 == "paper") {WinStatus = "lost";}
+                        if (UserAnswers[1].Item2 == "rock") {WinStatus = "tie";}
+                        break;
+                        case "paper":
+                        if (UserAnswers[1].Item2 == "scissors") {WinStatus = "lost";}
+                        if (UserAnswers[1].Item2 == "paper") {WinStatus = "tie";}
+                        break;
+                        case "scissors":
+                        if (UserAnswers[1].Item2 == "rock") {WinStatus = "lost";}
+                        if (UserAnswers[1].Item2 == "scissors") {WinStatus = "tie";}
+                        break;
+                    }
+                    if (WinStatus == "win")
+                    {
+                        message = $"[**{UserAnswers[0].Item1.Username}**] has won >> [**{UserAnswers[0].Item2}**]\n";
+                        message += $"[**{UserAnswers[1].Item1.Username}**] has lost >> [**{UserAnswers[1].Item2}**]\n";
+                        await component.UpdateAsync(x =>
+                        { 
+                            x.Content = $"{message}";
+                            x.Components = null;
+                        });
+                    } else if (WinStatus == "tie")
+                    {
+                        message = $"[**{UserAnswers[0].Item1.Username}**] >> [**{UserAnswers[0].Item2}**]\n";
+                        message += $"[**{UserAnswers[1].Item1.Username}**] >> [**{UserAnswers[1].Item2}**]\n";
+                        message += $"Its a tie!";
+                        var rockbuilder = new ComponentBuilder()
+                            .WithButton("Rematch!", "rematch-id");
+                        await component.UpdateAsync(x =>
+                        { 
+                            x.Content = $"{message}";
+                            x.Components = rockbuilder.Build();
+                        });
+                    } else
+                    {
+                        message = $"[**{UserAnswers[1].Item1.Username}**] has won >> [**{UserAnswers[1].Item2}**]\n";
+                        message += $"[**{UserAnswers[0].Item1.Username}**] has lost >> [**{UserAnswers[0].Item2}**]\n";
+                        await component.UpdateAsync(x =>
+                        { 
+                            x.Content = $"{message}";
+                            x.Components = null;
+                        });
+                    }
+
+                    UserAnswers.Clear();
+                } else 
+                {
+                    await component.UpdateAsync(x =>
+                    { 
+                        x.Content = $"{component.User.Username} has chosen!\n";
+                    });
+                }
+            }
             
         }
     }
